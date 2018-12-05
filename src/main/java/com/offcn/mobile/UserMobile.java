63bf885bcb7c370379231c529de83eb5bc0bcbd6
king <2607640909@qq.com>
2018-12-05 19:56:35 +0800

    老鸟新添加的函数

--- src/main/java/com/offcn/mobile/UserMobile.java
+++ src/main/java/com/offcn/mobile/UserMobile.java
@@ -5,4 +5,13 @@ public class UserMobile {
 	public void find() {
 		System.out.println("使用手机");
 	}
+	
+	/**
+	 * 
+	 *这是老鸟扩充的方法
+	 */
+	
+	public void findNum(){
+		System.out.println("查询电号号");
+	}
 }
