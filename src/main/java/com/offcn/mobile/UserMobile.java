8fe89038cb11dbd4c7e82f134395f4695e536d54
king <2607640909@qq.com>
2018-12-05 19:55:13 +0800

    小白新添加的函数

--- src/main/java/com/offcn/mobile/UserMobile.java
+++ src/main/java/com/offcn/mobile/UserMobile.java
@@ -5,4 +5,11 @@ public class UserMobile {
 	public void find() {
 		System.out.println("使用手机");
 	}
+	
+    /**
+     * 小白的扩充方法	
+     */
+	public void findNum() {
+		System.out.println("查询电话话");
+	}
 }
